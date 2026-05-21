1843ad6dcc60a3ac29fe54550bf19b01e0483eec
PineappleXJW <302409013@qq.com>
2026-05-22 01:33:06 +0800

    [2026-05-22]启用Windows通用控件6.0版本的视觉样式（XP风格/现代风格）

--- pch.h
+++ pch.h
@@ -9,5 +9,8 @@
 
 // 添加要在此处预编译的标头
 #include "framework.h"
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #endif //PCH_H
